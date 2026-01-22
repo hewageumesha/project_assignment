572c67f9405a878995aa58ceeaae72ce4734f2e3
dil <n.u.m.hewage@gmail.com>
2026-01-22 20:48:26 +0530

    setup project and create models

---
+++ src/main/java/com/project/assignment/model/Project.java
@@ -0,0 +1,38 @@
+package com.project.assignment.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "project")
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
